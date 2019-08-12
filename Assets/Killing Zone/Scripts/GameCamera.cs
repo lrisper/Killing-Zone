ada793091073f1e5d918fc298fd762d8b9b74e40
Lloyd Risper <lloydrisper@me.com>
2019-08-12 06:09:20 -0400

    Vertical Look

--- Assets/Killing Zone/Scripts/GameCamera.cs
+++ Assets/Killing Zone/Scripts/GameCamera.cs
@@ -9,6 +9,8 @@ public class GameCamera : MonoBehaviour
     [SerializeField] Vector3 _translationOffset;
     [SerializeField] Vector3 _followOffset;
 
+    float _verticalRotationAngle;
+
 
     // Start is called before the first frame update
     void Start()
@@ -19,9 +21,14 @@ public class GameCamera : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        //Camra look at target
         float yAngle = _target.transform.eulerAngles.y;
         Quaternion rotation = Quaternion.Euler(0, yAngle, 0);
         transform.position = _target.transform.position - (rotation * _followOffset);
         transform.LookAt(_target.transform.position + _translationOffset);
+
+        //Camera look up or down
+        _verticalRotationAngle += Input.GetAxis("Mouse Y");
+        transform.RotateAround(_target.transform.position, _target.transform.right, -_verticalRotationAngle);
     }
 }
