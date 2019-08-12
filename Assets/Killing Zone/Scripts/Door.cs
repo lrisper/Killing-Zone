8e2c0be012e2f4583afffce2c7ff6b046eef18d3
Lloyd Risper <lloydrisper@me.com>
2019-08-12 18:48:32 -0400

    Opening Door

---
+++ Assets/Killing Zone/Scripts/Door.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    [SerializeField] bool _opensInWards;
+    [SerializeField] float _openingSpeed;
+
+    bool _isOpen;
+    float _targetAngle;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion smoothRotation = Quaternion.Lerp(transform.localRotation, Quaternion.Euler(0, _targetAngle, 0), _openingSpeed * Time.deltaTime);
+        transform.localRotation = smoothRotation;
+    }
+
+    public void Interact()
+    {
+        _isOpen = !_isOpen;
+        if (_isOpen)
+        {
+            if (_opensInWards)
+            {
+                _targetAngle = -90f;
+            }
+            else
+            {
+                _targetAngle = 90f;
+            }
+        }
+        else
+        {
+            _targetAngle = 0;
+        }
+    }
+}
