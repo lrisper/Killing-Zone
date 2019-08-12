89029731ad25a72446c093f8410dec5eaf3ff002
Lloyd Risper <lloydrisper@me.com>
2019-08-12 02:16:05 -0400

    Camera Look

---
+++ Assets/Killing Zone/Scripts/GameCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameCamera : MonoBehaviour
+{
+    [SerializeField] GameObject target;
+    [SerializeField] Vector3 translationOffset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.LookAt(target.transform.position + translationOffset);
+    }
+}
