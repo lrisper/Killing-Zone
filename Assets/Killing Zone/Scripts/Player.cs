6fedc8758ebd9f21238a9d1d8e02b6aa935e0e15
Lloyd Risper <lloydrisper@me.com>
2019-08-12 05:57:22 -0400

    Focal Smoothnes

--- Assets/Killing Zone/Scripts/Player.cs
+++ Assets/Killing Zone/Scripts/Player.cs
@@ -8,6 +8,7 @@ public class Player : MonoBehaviour
     [SerializeField] GameObject _focalPoint;
     [SerializeField] float _focalDistance;
     [SerializeField] KeyCode _changefocalSideKey;
+    [SerializeField] float _focalSmoothness = 7.5f;
 
     bool _isFocalPointOnLeft = true;
 
@@ -26,6 +27,7 @@ public class Player : MonoBehaviour
         }
 
         float targetX = _focalDistance * (_isFocalPointOnLeft ? -1 : 1);
-        _focalPoint.transform.localPosition = new Vector3(targetX, _focalPoint.transform.localPosition.y, _focalPoint.transform.localPosition.z);
+        float smoothX = Mathf.Lerp(_focalPoint.transform.localPosition.x, targetX, _focalSmoothness * Time.deltaTime);
+        _focalPoint.transform.localPosition = new Vector3(smoothX, _focalPoint.transform.localPosition.y, _focalPoint.transform.localPosition.z);
     }
 }
