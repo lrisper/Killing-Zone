e590a03ea9beff0519e04bbf4276a5e4ca710461
Lloyd Risper <lloydrisper@me.com>
2019-08-12 02:37:37 -0400

    Camera Move

--- Assets/Killing Zone/Scripts/GameCamera.cs
+++ Assets/Killing Zone/Scripts/GameCamera.cs
@@ -6,6 +6,7 @@ public class GameCamera : MonoBehaviour
 {
     [SerializeField] GameObject target;
     [SerializeField] Vector3 translationOffset;
+    [SerializeField] Vector3 followOffset;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,9 @@ public class GameCamera : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        float yAngle = target.transform.eulerAngles.y;
+        Quaternion rotation = Quaternion.Euler(0, yAngle, 0);
+        transform.position = target.transform.position - (rotation * followOffset);
         transform.LookAt(target.transform.position + translationOffset);
     }
 }
