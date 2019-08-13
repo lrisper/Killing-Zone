186155537673f90cbc33562203379127bf600276
Lloyd Risper <lloydrisper@me.com>
2019-08-12 22:03:58 -0400

    Raycast

--- Assets/Killing Zone/Scripts/Player.cs
+++ Assets/Killing Zone/Scripts/Player.cs
@@ -10,6 +10,11 @@ public class Player : MonoBehaviour
     [SerializeField] KeyCode _changefocalSideKey;
     [SerializeField] float _focalSmoothness = 7.5f;
 
+    [Header("Interaction")]
+    [SerializeField] GameCamera _gameCamera;
+    [SerializeField] KeyCode _interactionKey;
+    [SerializeField] float _interactionDistance;
+
     bool _isFocalPointOnLeft = true;
 
     // Start is called before the first frame update
@@ -29,5 +34,20 @@ public class Player : MonoBehaviour
         float targetX = _focalDistance * (_isFocalPointOnLeft ? -1 : 1);
         float smoothX = Mathf.Lerp(_focalPoint.transform.localPosition.x, targetX, _focalSmoothness * Time.deltaTime);
         _focalPoint.transform.localPosition = new Vector3(smoothX, _focalPoint.transform.localPosition.y, _focalPoint.transform.localPosition.z);
+
+        // interaction logic
+#if UNITY_EDITOR
+        //Draw interaction line
+        Debug.DrawLine(_gameCamera.transform.position, _gameCamera.transform.position + _gameCamera.transform.forward * _interactionDistance, Color.green);
+#endif
+        if (Input.GetKeyDown(_interactionKey))
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(_gameCamera.transform.position, _gameCamera.transform.forward, out hit, _interactionDistance))
+            {
+
+            }
+        }
+
     }
 }
