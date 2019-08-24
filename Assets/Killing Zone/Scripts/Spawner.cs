7909467869193a6b6519d8d444ac86ff97704f6b
Lloyd Risper <lloydrisper@me.com>
2019-08-24 18:57:10 -0400

    Spawning Items

---
+++ Assets/Killing Zone/Scripts/Spawner.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Spawner : NetworkBehaviour
+{
+    [SerializeField] private GameObject _prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject instance = Instantiate(_prefab, transform.position, transform.rotation, transform.parent);
+
+        NetworkServer.Spawn(instance);
+
+        Destroy(gameObject);
+    }
+
+
+}
